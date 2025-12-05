fd6946c91740b6e60aec113bd17afd1a3f0fd39a
Rafael <efael1986@gmail.com>
2025-12-04 23:27:55 -0300

    first commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,42 @@
+import {
+    Injectable,
+    OnModuleInit,
+    OnModuleDestroy,
+    Logger,
+} from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+@Injectable()
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+{
+    private pool: Pool;
+    private readonly logger = new Logger(PrismaService.name);
+
+    constructor() {
+        const pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+            max: 3,
+            min: 0,
+            idleTimeoutMillis: 5000,
+        });
+
+        const adapter = new PrismaPg(pool);
+        super({ adapter });
+        this.pool = pool;
+    }
+
+    async onModuleInit() {
+        this.logger.log('Conectando ao banco de dados');
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        this.logger.log('Desconectando do bando de dados');
+        await this.$disconnect();
+        await this.pool.end();
+    }
+}
