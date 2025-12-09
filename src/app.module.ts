ef1689ef65a4922c54fc58dd58b33f32cc7842f8
Rafael Vasconcelos Fernandes <efael1986@gmail.com>
2025-12-09 14:48:18 -0400

    auth module whith public decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
     imports: [PrismaModule, AuthModule],
     controllers: [],
-    providers: [],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: JwtAuthGuard,
+        }
+    ],
 })
 export class AppModule {}
