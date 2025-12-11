21cfc629e4ff68f42b33ba0994949eab8189e098
Rafael Vasconcelos <efael1986@gmail.com>
2025-12-11 02:39:35 -0400

    edit formater prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
         {
             provide: APP_GUARD,
             useClass: JwtAuthGuard,
-        }
+        },
     ],
 })
 export class AppModule {}
